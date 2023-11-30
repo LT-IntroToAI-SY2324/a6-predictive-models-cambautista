d628ee1418077294607aa7f4a4fc5102cfa8dc66
Camila <cbautista24@cps.edu>
2023-11-30 11:29:45 -0600

    added the diabetes ex from class

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -11,14 +11,24 @@ y = data["Blood Pressure"].values
 x = x.reshape(-1,1)
 
 # Create the model
+model = LinearRegression().fit(x, y)
 
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
 
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
+print(coef, intercept, r_squared)
+
 
 # Print out the linear equation and r squared value
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+
 
 # Predict the the blood pressure of someone who is 43 years old.
+x_predict= 43
 # Print out the prediction
-
+prediction = model.predict([[x_predict]])
 # Create the model in matplotlib and include the line of best fit
