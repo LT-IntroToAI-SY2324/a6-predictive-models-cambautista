52e5550c563cc96fb31adc06b453b45438024142
Camila <cbautista24@cps.edu>
2023-12-04 11:34:05 -0600

    commiting in case

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 **********CREATE THE MODEL**********
 '''
 
-data = pd.read_csv("data.csv")
+data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
 x = data["Age"].values
 y = data["Blood Pressure"].values
 
