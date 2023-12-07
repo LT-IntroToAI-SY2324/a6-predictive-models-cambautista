45ddc869c1f4dac70d3beaa891467d094e05864f
Camila <cbautista24@cps.edu>
2023-12-07 11:30:57 -0600

    edited part 3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,11 +9,14 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 #create linear regression model
-
+model = LinearRegression().fit(xtrain, ytrain)
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
 
 
 #Loop through the data and print out the predicted prices and the 
@@ -21,6 +24,15 @@ y = data["Price"].values
 print("***************")
 print("Testing Results")
 
-
+ 
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
+print(predict)
+print("\nTesting Multivariable Model with Testing Data:")
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the ytest dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the xtest dataset
+    print(f": {x_coord[0]} : {x_coord[1]} : {x_coord[2]} Actual: {actual} Predicted: {predicted_y}")
 
 #use 24 miles(000)
