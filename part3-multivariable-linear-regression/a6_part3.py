a416cb33215bb9f1810f035dc479a2a153bbbb86
Camila <cbautista24@cps.edu>
2023-12-11 11:32:30 -0600

    part3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -10,29 +10,35 @@ y = data["Price"].values
 
 #split the data into training and testing data
 xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
+
 #create linear regression model
 model = LinearRegression().fit(xtrain, ytrain)
+
+
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
 coef = np.around(model.coef_, 2)
 intercept = round(float(model.intercept_), 2)
 r_squared = round(model.score(x, y),2)
+print(f"Model's Linear Equation: y={coef[0]}x1 + {coef[1]}x2 + {intercept}")
+print("R Squared value:", r_squared)
+print("***************")
+print("Testing Results")
+
 
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
-print("***************")
-print("Testing Results")
-
- 
-predict = model.predict(xtest)
-predict = np.around(predict, 2)
+predictions = model.predict(xtest)
+predict=np.around(predictions , 2)
 print(predict)
-print("\nTesting Multivariable Model with Testing Data:")
-for index in range(len(xtest)):
-    actual = ytest[index] # gets the actual y value from the ytest dataset
-    predicted_y = predict[index] # gets the predicted y value from the predict variable
-    x_coord = xtest[index] # gets the x value from the xtest dataset
-    print(f": {x_coord[0]} : {x_coord[1]} : {x_coord[2]} Actual: {actual} Predicted: {predicted_y}")
 
-#use 24 miles(000)
+for index in range(len(xtest)):
+    actual = round(ytest[index])
+    predicted_y = predict[index] 
+    x_coord = xtest[index]
+    x_coord=np.around(x_coord,2)
+    
+    print(f"miles: {x_coord[0]} age: {x_coord[1]} Actual: {actual} Predicted: {predicted_y}")
+print(f"miles: {x_coord[89]} age: {x_coord[10]} Actual: {actual} Predicted: {predicted_y}")
+print(f"miles: {x_coord[150]} age: {x_coord[20]} Actual: {actual} Predicted: {predicted_y}")
